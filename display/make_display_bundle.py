42a89090bd10002fb7b9f287d0dc163a60fc00ce
Neha Kennard <nnayak@umass.edu>
2024-01-29 16:18:11 -0500

    update display code

--- display/make_display_bundle.py
+++ display/make_display_bundle.py
@@ -10,11 +10,15 @@ parser = argparse.ArgumentParser(description="")
 parser.add_argument("-d", "--data_dir", default="", type=str, help="")
 parser.add_argument("-o",
                     "--output_dir",
-                    default="v2/bundles/",
+                    default="v3/bundles/",
                     type=str,
                     help="")
 
 CONTEXT_SIZE = 20
+
+OLD_START, OLD_END, NEW_START, NEW_END = [f"<{label}>"
+    #for label in "old /old new /new".split()]
+    for label in "diffdel /diffdel diffadd /diffadd".split()]
 
 
 def get_all_tokens(diff):
@@ -47,6 +51,41 @@ def get_diff_text(diff, flat_initial_tokens):
     ])
 
 
+def save_raw_samples(output_dir, data_dir, forum):
+    raw_dir = f'{output_dir}/raw_{forum}'
+    shutil.copytree(f'{data_dir}/{forum}/', raw_dir)
+    with tarfile.open(
+          os.path.normpath(f'{output_dir}/raw_{forum}.tgz'),
+          "w:gz") as tar:
+        tar.add(raw_dir, arcname=f'raw_{forum}/')
+    shutil.rmtree(raw_dir)
+
+def highlight_diffs(diffs_obj):
+
+    flat_initial_tokens = sum(diffs_obj['tokens']['initial'], [])
+    first_diff_start = diff_objs['diffs'][0]['location']
+    highlighted_tokens = flat_initial_tokens[:first_diff_start]
+
+    diff_anchors = []
+    new_i = len(highlighted_tokens)
+    for diff in diffs_obj['diffs']:
+        start = len(highlighted_tokens)
+        old, new = get_all_tokens(diff)
+        if old:
+            highlighted_tokens += [OLD_START] + old + [OLD_END]
+        if new:
+            highlighted_tokens += [NEW_START] + new + [NEW_END]
+        end = len(highlighted_tokens)
+        diff_anchors.append((star, end, diff))
+
+    final_diff =  diff_objs['diffs'][-1]
+    final_diff_old, _ = get_all_tokens(final_diff)
+    final_diff_end = final_diff['location'] + len(final_diff_old)
+    highlighted_tokens += flat_initial_tokens[final_diff_end:]
+
+    return highlighted_tokens, diff_anchors
+
+
 def main():
 
     args = parser.parse_args()
@@ -57,49 +96,53 @@ def main():
     for diff_dir in sorted(glob.glob(f"{args.data_dir}/*/"))[:10]:
         forum = diff_dir.split("/")[-2]
 
-        # Just for printing samples
-        # raw_dir = f'{args.output_dir}/raw_{forum}'
-        # shutil.copytree(f'{args.data_dir}/{forum}/', raw_dir)
-        # with tarfile.open(
-        #       os.path.normpath(f'{args.output_dir}/raw_{forum}.tgz'),
-        #       "w:gz") as tar:
-        #     tar.add(raw_dir, arcname=f'raw_{forum}/')
-        # shutil.rmtree(raw_dir)
-        # continue
+        #save_raw_samples(args.output_dir, args.data_dir, forum)
+        #continue
 
-        os.makedirs(f'{args.output_dir}/bundle_{forum}', exist_ok=True)
         with open(f'{args.data_dir}/{forum}/diffs.json', 'r') as f:
             obj = json.load(f)
-            initial_tokens = obj['tokens']['initial']
-            flat_initial_tokens = sum(initial_tokens, [])
-            diff_index = 1
-            for diff in tqdm.tqdm(obj['diffs']):
-                diff_identifier = f'{forum}|||{diff_index}'
-                diff_text = get_diff_text(diff, flat_initial_tokens)
-                html_text = str(template_text).replace(
-                    "DIFF_IDENTIFIER",
-                    diff_identifier).replace("DIFF_TEXT", diff_text).replace(
-                        "PREV_INDEX", str(diff_index - 1)).replace(
-                            "NEXT_INDEX", str(diff_index + 1)).replace(
-                                "ICLR_LINK",
-                                f"https://openreview.net/forum?id={forum}")
-                with open(
-                        f'{args.output_dir}/bundle_{forum}/{diff_index}.html',
-                        'w') as f:
-                    f.write(html_text)
-                diff_index += 1
-            for aux_file in ["style.css", "0.html", "last.html"]:
-                shutil.copyfile(
-                    f"templates/{aux_file}",
-                    f'{args.output_dir}/bundle_{forum}/{aux_file}')
-            shutil.move(f'{args.output_dir}/bundle_{forum}/last.html',
-                        f'{args.output_dir}/bundle_{forum}/{diff_index}.html')
-            source_dir = f'{args.output_dir}/bundle_{forum}/'
-            with tarfile.open(
+
+        highlighted_tokens, diff_map = highlight_diffs(obj)
+
+        with open('blerp.html', 'w') as f:
+            html_text = str(template_text).replace("DIFF_TEXT",
+                " ".join(highlighted_tokens))
+            f.write(html_text)
+
+        dsds
+
+        os.makedirs(f'{args.output_dir}/bundle_{forum}', exist_ok=True)
+        initial_tokens = obj['tokens']['initial']
+        flat_initial_tokens = sum(initial_tokens, [])
+        diff_index = 1
+        for diff in tqdm.tqdm(obj['diffs']):
+            diff_identifier = f'{forum}|||{diff_index}'
+            diff_text = get_diff_text(diff, flat_initial_tokens)
+            html_text = str(template_text).replace(
+                "DIFF_IDENTIFIER",
+                diff_identifier).replace("DIFF_TEXT", diff_text).replace(
+                    "PREV_INDEX", str(diff_index - 1)).replace(
+                        "NEXT_INDEX", str(diff_index + 1)).replace(
+                            "ICLR_LINK",
+                            f"https://openreview.net/forum?id={forum}")
+            with open(
+                    f'{args.output_dir}/bundle_{forum}/{diff_index}.html',
+                    'w') as f:
+                f.write(html_text)
+            diff_index += 1
+        for aux_file in ["style.css", "0.html", "last.html"]:
+            shutil.copyfile(
+                f"templates/{aux_file}",
+                f'{args.output_dir}/bundle_{forum}/{aux_file}')
+        shutil.move(f'{args.output_dir}/bundle_{forum}/last.html',
+                    f'{args.output_dir}/bundle_{forum}/{diff_index}.html')
+        source_dir = f'{args.output_dir}/bundle_{forum}/'
+        with tarfile.open(
                     os.path.normpath(f'{args.output_dir}/bundle_{forum}.tgz'),
                     "w:gz") as tar:
                 tar.add(source_dir, arcname=os.path.basename(source_dir))
-            shutil.rmtree(source_dir)
+        shutil.rmtree(source_dir)
+
 
 
 if __name__ == "__main__":
