9f60d90652049ee6ca07610f81c7f5bfcc734a3d
Neha Kennard <nnayak@umass.edu>
2024-02-08 21:26:34 +0000

    sciencebeam parsing

--- 07_merge_to_sentence_level.py
+++ 07_merge_to_sentence_level.py
@@ -1,12 +1,21 @@
+import argparse
 import collections
+import glob
 import json
+import os
 import sys
+import tqdm
 
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("-d", "--data_dir", default="", type=str, help="")
 
 def build_sentence_map(sentences):
     sentence_index_map = []
     for i, sentence in enumerate(sentences):
         sentence_index_map += [i] * len(sentence)
+    sentence_index_map.append(sentence_index_map[-1] + 1)
+    # Placeholder in case something is added at the end of the initial text
+    # Shouldn't cause any problems, usually ignored
     return sentence_index_map
 
 
@@ -20,28 +29,33 @@ def get_all_tokens(diff):
     return old, new
 
 
-def main():
+def clean_diff(diff):
+    if diff['old'] is None:
+        diff['old'] = []
+    if diff['new'] is None:
+        diff['new'] = []
+    return diff
 
-    diff_file = sys.argv[1]
-    with open(diff_file, 'r') as f:
-        obj = json.load(f)
-
-        flat_initial_tokens = sum(obj['tokens']['initial'], [])
-        sentence_index_map = build_sentence_map(obj['tokens']['initial'])
-
-        print(len(obj['diffs']))
-        print()
-        sentencewise_collector = collections.defaultdict(list)
-        for diff in obj['diffs']:
-            sent_index = sentence_index_map[diff['location']]
-            sentencewise_collector[sent_index].append(diff)
+def merge_diffs(original_diffs):
+    flat_initial_tokens = sum(original_diffs['tokens']['initial'], [])
+    if not flat_initial_tokens:
+        return None
+    sentence_index_map = build_sentence_map(original_diffs['tokens']['initial'])
+    #print(len(flat_initial_tokens), len(sentence_index_map))
+    sentencewise_collector = collections.defaultdict(list)
+    for diff in original_diffs['diffs']:
+        #print(diff['location'])
+        sent_index = sentence_index_map[diff['location']]
+        sentencewise_collector[sent_index].append(diff)
 
     new_diffs = []
+
     for sentence_index in sorted(sentencewise_collector.keys()):
         this_sentence_diffs = sentencewise_collector[sentence_index]
 
-        if len(this_sentence_diffs) == 1:
-            new_diffs += this_sentence_diffs
+        if len(this_sentence_diffs) == 1: # nothing to merge
+            only_diff, = this_sentence_diffs
+            new_diffs.append(clean_diff(only_diff))
             continue
 
         # Multiple diffs in this sentence
@@ -63,18 +77,38 @@ def main():
                 end_location_of_current_diff:old_location_of_next_diff]
             merged_old += in_between_part
             merged_new += in_between_part
+
         new_diffs.append({
             "location": this_sentence_diffs[0]['location'],
             'old': merged_old,
             'new': merged_new
         })
 
-    print(len(new_diffs))
-    final_diffs_obj = {'tokens': obj['tokens'], 'diffs': new_diffs}
+    return {'tokens': original_diffs['tokens'], 'diffs': new_diffs}
 
-    with open('final_diffs.json', 'w') as f:
-        json.dump(final_diffs_obj, f)
 
+
+def main():
+
+    args = parser.parse_args()
+
+    for diff_dir in tqdm.tqdm(glob.glob(f'{args.data_dir}/*')):
+        diff_file = f'{diff_dir}/diffs.json'
+        merged_file = f'{diff_dir}/merged_diffs.json'
+
+        # Only if merged diffs file doesn't exist
+        if os.path.exists(merged_file):
+            continue
+        else:
+            with open(diff_file, 'r') as f:
+                obj = json.load(f)
+            #print(diff_file)
+            new_obj = merge_diffs(obj)
+            if new_obj is not None:
+                with open(f'{diff_dir}/merged_diffs.json', 'w') as f:
+                    json.dump(new_obj, f)
+            else:
+                print("Error merging ", diff_file)
 
 if __name__ == "__main__":
     main()
---
+++ sciencebeam/parse_iclr_pdfs.py
@@ -0,0 +1,86 @@
+from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from os import devnull
+
+from sciencebeam_parser.resources.default_config import DEFAULT_CONFIG_FILE
+from sciencebeam_parser.config.config import AppConfig
+from sciencebeam_parser.utils.media_types import MediaTypes
+from sciencebeam_parser.app.parser import ScienceBeamParser
+
+import collections
+import glob
+import json
+import tqdm
+import xml.etree.ElementTree as ET
+
+PREFIX = "{http://www.tei-c.org/ns/1.0}"
+TEXT_ID = f"{PREFIX}text"
+BODY_ID = f"{PREFIX}body"
+DIV_ID = f"{PREFIX}div"
+HEAD_ID = f"{PREFIX}head"
+P_ID = f"{PREFIX}p"
+NOTE_ID = f"{PREFIX}note"
+FORMULA_ID = f'{PREFIX}formula'
+
+Section = collections.namedtuple("Section", "title number text".split())
+
+
+class Section(object):
+    def __init__(self, title, number, text):
+        self.title = title
+        self.number = number
+        self.text = text
+
+    def as_json(self):
+        return {'title': self.title, 'number': self.number, 'text': self.text}
+
+
+def parse_xml(filename):
+    sections = []
+
+    divs = (ET.parse(filename).getroot().findall(TEXT_ID)[0].findall(BODY_ID)
+            [0].findall(DIV_ID))
+    for div in divs:
+        (header_node, ) = div.findall(HEAD_ID)
+        maybe_section_number = header_node.attrib.get('n', None)
+        maybe_section_title = header_node.text
+
+        text = []
+        for p in div.findall("*"):
+            if p.tag in [P_ID, NOTE_ID]:
+                text.append(" ".join(p.itertext()))
+            if p.tag == FORMULA_ID:
+                text.append("$$FORMULA$$")
+
+        if maybe_section_number is None:
+            sections[-1].text += [maybe_section_title]
+            sections[-1].text += text
+            pass
+        else:
+            sections.append(
+                Section(maybe_section_title, maybe_section_number, text))
+
+    return [section.as_json() for section in sections]
+
+
+def main():
+    config = AppConfig.load_yaml(DEFAULT_CONFIG_FILE)
+    sciencebeam_parser = ScienceBeamParser.from_config(config)
+
+    for initial_filename in tqdm.tqdm(
+        list(
+        glob.glob('/gypsum/work1/mccallum/nnayak/forums/*/initial.pdf'))[:10]):
+
+        for filename in [initial_filename,
+            initial_filename.replace('initial.pdf', 'final.pdf')]:
+            output_filename = filename.replace('.pdf', '_sbraw.json')
+
+            with sciencebeam_parser.get_new_session() as session:
+                session_source = session.get_source(
+                    filename, MediaTypes.PDF)
+                converted_file = session_source.get_local_file_for_response_media_type(
+                    MediaTypes.TEI_XML)
+                with open(output_filename, 'w') as f:
+                    json.dump(parse_xml(converted_file), f)
+
+if __name__ == "__main__":
+    main()
